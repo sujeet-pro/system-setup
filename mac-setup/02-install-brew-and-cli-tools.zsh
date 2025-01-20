c510141ac38bff7e376612a64d1b101bd45754b9
Sujeet Jaiswal <sujeet.profession@gmail.com>
2025-01-20 12:40:56 +0530

    chore: sync

--- mac-setup/02-install-brew-and-cli-tools.zsh
+++ mac-setup/02-install-brew-and-cli-tools.zsh
@@ -1,5 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew install btop # System Monitor
 brew install eza # ls replacement
 brew install wget # Download files
 brew install curl # Download files
--- mac-setup/03-brew-cask-install.zsh
+++ mac-setup/03-brew-cask-install.zsh
@@ -1,13 +1,25 @@
+# Browsers
 brew install --cask google-chrome # Browser
 brew install --cask brave-browser # Browser
 brew install --cask vivaldi # Browser
 
+# Terminals
+brew install --cask ghostty # Terminal
+brew install --cask warp
+
+# Code Editors
 brew install --cask visual-studio-code # Code Editor
 brew install --cask zed # Code Editor
 brew install --cask sublime-text  # Code Editor
 brew install --cask intellij-idea-ce # Code Editor
 brew install --cask pycharm-ce # Code Editor
 
+# Dev Tools
+brew install --cask dbeaver-community # Database Manager
+brew install --cask postman # API Testing
+
+# Utilities
+brew install --cask chatgpt # AI Chatbot
 brew install --cask maccy # Clipboard Manager
 brew install --cask rectangle # Window Manager
 
@@ -15,7 +27,8 @@ brew install --cask slack # Communication
 brew install --cask zoom # Communication
 
 brew install --cask nordvpn # VPN
-brew install --cask postman # API Testing
 
 brew install --cask obsidian # Note Taking
 brew install --cask zotero # Research Paper Manager
+
+brew install --cask bitwarden # Password Manager
